ff59ff0d6f283200183da2b022f8d86d518d958d
Mikkel Malmberg <mikkel@brnbw.com>
2017-11-26 14:16:42 +0100

    Inherit from main app's ApplicationController

--- app/controllers/passwordless/application_controller.rb
+++ app/controllers/passwordless/application_controller.rb
@@ -1,5 +1,4 @@
 module Passwordless
-  class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  class ApplicationController < ::ApplicationController
   end
 end
