afe38554be3de9b4d2e6499243dedf50fe1ee89e
Mikkel Malmberg <mikkel@brnbw.com>
2017-12-14 23:30:09 +0100

    Add yard and rubocop gems

--- lib/passwordless.rb
+++ lib/passwordless.rb
@@ -1,6 +1,7 @@
 require 'passwordless/engine'
 require 'passwordless/url_safe_base_64_generator'
 
+# The main Passwordless module
 module Passwordless
   mattr_accessor(:default_from_address) { 'CHANGE_ME@example.com' }
   mattr_accessor(:token_generator) do
