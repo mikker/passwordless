698a71b74beeb0ae2184b8b677a6ea3bff348d43
Piotr Usewicz <piotr@layer22.com>
2018-06-06 09:21:56 +0200

    Add as: :authenticatable to sessions association (#24)

---
+++ test/models/authenticatable_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class AuthenticatableTest < ActiveSupport::TestCase
+  test '#passwordless_sessions' do
+    user = users(:john)
+    assert_equal [passwordless_sessions(:john)], user.passwordless_sessions
+  end
+end
