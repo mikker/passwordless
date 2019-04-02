38d94212ab5bd32d5033500417512dbda2b5691b
Aurélien Malisart <aurelien.malisart@gmail.com>
2019-04-02 17:31:04 +0200

    Pass request to

--- app/controllers/passwordless/sessions_controller.rb
+++ app/controllers/passwordless/sessions_controller.rb
@@ -26,7 +26,7 @@ module Passwordless
       session = build_passwordless_session(find_authenticatable)
 
       if session.save
-        Passwordless.after_session_save.call(session)
+        Passwordless.after_session_save.call(session, request)
       end
 
       render
