685460c1845a8611ba2761bfc608debf85d8d089
Devindra <arden.ived@gmail.com>
2019-11-04 20:56:10 +0000

    Allow more fine customization of redirect path

--- app/controllers/passwordless/sessions_controller.rb
+++ app/controllers/passwordless/sessions_controller.rb
@@ -44,13 +44,13 @@ module Passwordless
       BCrypt::Password.create(params[:token])
       sign_in passwordless_session
 
-      redirect_to passwordless_redirect_path || passwordless_default_redirect_path
+      redirect_to passwordless_success_redirect_path
     rescue Errors::TokenAlreadyClaimedError
       flash[:error] = I18n.t(".passwordless.sessions.create.token_claimed")
-      redirect_to passwordless_default_redirect_path
+      redirect_to passwordless_failure_redirect_path
     rescue Errors::SessionTimedOutError
       flash[:error] = I18n.t(".passwordless.sessions.create.session_expired")
-      redirect_to passwordless_default_redirect_path
+      redirect_to passwordless_failure_redirect_path
     end
 
     # match '/sign_out', via: %i[get delete].
@@ -58,20 +58,24 @@ module Passwordless
     # @see ControllerHelpers#sign_out
     def destroy
       sign_out authenticatable_class
-      redirect_to passwordless_default_redirect_path
+      redirect_to passwordless_sign_out_redirect_path
     end
 
     protected
 
-    def passwordless_default_redirect_path
-      Passwordless.default_redirect_path
+    def passwordless_sign_out_redirect_path
+      Passwordless.sign_out_redirect_path
     end
 
-    def passwordless_redirect_path
+    def passwordless_failure_redirect_path
+      Passwordless.failure_redirect_path
+    end
+
+    def passwordless_success_redirect_path
       return nil unless Passwordless.redirect_back_after_sign_in
 
       redirect_url = reset_passwordless_redirect_location!(authenticatable_class)
-      params[:url] || redirect_url
+      params[:url] || redirect_url || Passwordless.success_redirect_path
     end
 
     private
