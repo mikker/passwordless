e1726308577c4abf4b1c074c3a5d860dbbccd8e5
Mikkel Malmberg <mikkel@brnbw.com>
2019-12-19 14:40:29 +0100

    v0.9.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 0.9.0 (2019-12-19)
 
 ### Added
 
--- lib/passwordless/version.rb
+++ lib/passwordless/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Passwordless
-  VERSION = "0.8.2" # :nodoc:
+  VERSION = "0.9.0" # :nodoc:
 end
