8bd63f41c33575ce96277f1d70e7564b6fa5e765
Mikkel Malmberg <mikkel@brnbw.com>
2021-04-29 10:48:10 +0200

    Upgrade dummy app to Rails 6.1

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,30 +1,37 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative("boot")
-
+require "rails"
 # Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
+require "active_model/railtie"
 require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
 # require "action_cable/engine"
+# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-# require "sprockets/railtie"
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "passwordless"
 
 module Dummy
   class Application < Rails::Application
-    config.active_record.sqlite3.represent_boolean_as_integer = true
-
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.action_mailer.default_url_options = {host: "localhost:3000"}
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.action_mailer.default_url_options = { host: 'example.com' }
   end
 end
