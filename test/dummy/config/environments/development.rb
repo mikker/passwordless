e988187eed9617b1663f57215f22805290afd2e0
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2021-12-19 18:00:03 +0900

    use ActiveSupport::FileUpdateChecker in the development environment.
    The `ActiveSupport::EventedFileUpdateChecker` depends on listen, but since it is not installed,
    an exception is thrown when trying to start the development environment.
    
    ```
    bin/rails s
    => Booting WEBrick
    => Rails 6.1.4.1 application starting in development http://localhost:3000
    => Run `bin/rails server --help` for more startup options
    Exiting
    passwordless/vendor/bundle/ruby/3.0.0/gems/zeitwerk-2.5.1/lib/zeitwerk/kernel.rb:35:in `require': cannot load such file -- listen (LoadError)
    ```
    
    Therefore, use `ActiveSupport::FileUpdateChecker` which does not depend on listen.
    
    > config.file_watcher
    > Rails ships with ActiveSupport::FileUpdateChecker, the default
    > https://guides.rubyonrails.org/v6.1/configuring.html#rails-general-configuration

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -58,8 +58,8 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # routes, locales, etc.
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
