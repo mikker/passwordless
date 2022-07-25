584954a89b7655aa1d6cfe30d92fcd06455250e9
Nick Hammond <nick@nickhammond.com>
2022-07-25 10:23:01 -0700

    Add a simple view generator for #64

---
+++ lib/generators/passwordless/views_generator.rb
@@ -0,0 +1,15 @@
+require 'rails/generators'
+
+module Passwordless
+  module Generators
+    class ViewsGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../../app/views/passwordless', __dir__)
+
+      def install
+        copy_file 'mailer/magic_link.text.erb', 'app/views/passwordless/mailer/magic_link.text.erb'
+        copy_file 'sessions/new.html.erb', 'app/views/passwordless/sessions/new.html.erb'
+        copy_file 'sessions/create.html.erb', 'app/views/passwordless/sessions.create.html.erb'
+      end
+    end
+  end
+end
