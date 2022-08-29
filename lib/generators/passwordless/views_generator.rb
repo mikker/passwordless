568e41f1a18ea6fa78f400fe160b15668a42abff
Mikkel Malmberg <mikkel@brnbw.com>
2022-08-29 10:36:34 +0200

    Fix typo in generator

--- lib/generators/passwordless/views_generator.rb
+++ lib/generators/passwordless/views_generator.rb
@@ -8,7 +8,7 @@ module Passwordless
       def install
         copy_file 'mailer/magic_link.text.erb', 'app/views/passwordless/mailer/magic_link.text.erb'
         copy_file 'sessions/new.html.erb', 'app/views/passwordless/sessions/new.html.erb'
-        copy_file 'sessions/create.html.erb', 'app/views/passwordless/sessions.create.html.erb'
+        copy_file 'sessions/create.html.erb', 'app/views/passwordless/sessions/create.html.erb'
       end
     end
   end
