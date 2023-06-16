1e9eaca9b267ac7ce7205ca1d308ed610ece35fd
Mikkel Malmberg <mikkel@brnbw.com>
2023-06-16 12:24:04 +0200

    Test: Don't load defaults as old ruby versions load old Rails versions

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -24,19 +24,8 @@ Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
     routes.default_url_options[:host] = "localhost:3000"
     config.action_mailer.default_url_options = {host: "localhost", port: "3000"}
     config.hosts << "www.example.com"
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
