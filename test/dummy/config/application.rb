43d9e48246017128499d5407a43f95243304e63e
Mikkel Malmberg <mikkel@brnbw.com>
2023-06-16 10:31:27 +0200

    Update test/dummy app

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,18 +1,21 @@
 require_relative "boot"
 
 require "rails"
+
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+
 # require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
+
 # require "action_cable/engine"
-# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,6 +24,9 @@ Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -28,7 +34,5 @@ module Dummy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.action_mailer.default_url_options = { host: 'example.com' }
   end
 end
