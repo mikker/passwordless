012be83d4cf0142311c90a757215e94852a37168
Mikkel Malmberg <mikkel@brnbw.com>
2023-06-16 14:13:05 +0200

    Add test for multiple mounts

--- test/passwordless_for_test.rb
+++ test/passwordless_for_test.rb
@@ -50,5 +50,52 @@ module Passwordless
         {authenticatable: "user", resource: "users"}
       )
     end
+
+    test("map sign in for admin") do
+      assert_recognizes(
+        {
+          controller: "passwordless/sessions",
+          action: "new",
+          authenticatable: "admin",
+          resource: "admins"
+        },
+        {method: :get, path: "/admins/sign_in"},
+        {authenticatable: "admin", resource: "admins"}
+      )
+
+      assert_recognizes(
+        {
+          controller: "passwordless/sessions",
+          action: "create",
+          authenticatable: "admin",
+          resource: "admins"
+        },
+        {method: :post, path: "/admins/sign_in", params: {passwordless: {email: "a@a"}}},
+        {authenticatable: "admin", resource: "admins"}
+      )
+
+      assert_recognizes(
+        {
+          controller: "passwordless/sessions",
+          action: "show",
+          authenticatable: "admin",
+          resource: "admins",
+          token: "abc123"
+        },
+        {method: :get, path: "/admins/sign_in/abc123", params: {token: "abc123"}},
+        {authenticatable: "admin", resource: "admins"}
+      )
+
+      assert_recognizes(
+        {
+          controller: "passwordless/sessions",
+          action: "destroy",
+          authenticatable: "admin",
+          resource: "admins"
+        },
+        {method: :delete, path: "/admins/sign_out"},
+        {authenticatable: "admin", resource: "admins"}
+      )
+    end
   end
 end
