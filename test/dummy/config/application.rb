5816b7e7598f43e914ba7877ce129507db5de349
Mikkel Malmberg <mikkel@brnbw.com>
2023-06-16 10:41:53 +0200

    Test: Add :host info for routes and mailers

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -27,6 +27,9 @@ module Dummy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    routes.default_url_options[:host] = "localhost:3000"
+    config.action_mailer.default_url_options = {host: "localhost", port: "3000"}
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
