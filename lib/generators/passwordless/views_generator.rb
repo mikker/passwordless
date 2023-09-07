851def915c84f6052bc9977f2612e4b97238553d
Mikkel Malmberg <mikkel@brnbw.com>
2023-09-07 22:16:27 +0200

    Fix view generator

--- lib/generators/passwordless/views_generator.rb
+++ lib/generators/passwordless/views_generator.rb
@@ -6,8 +6,9 @@ module Passwordless
       source_root File.expand_path("../../../app/views/passwordless", __dir__)
 
       def install
-        copy_file("mailer/magic_link.text.erb", "app/views/passwordless/mailer/magic_link.text.erb")
+        copy_file("mailer/sign_in.text.erb", "app/views/passwordless/mailer/sign_in.text.erb")
         copy_file("sessions/new.html.erb", "app/views/passwordless/sessions/new.html.erb")
+        copy_file("sessions/show.html.erb", "app/views/passwordless/sessions/show.html.erb")
       end
     end
   end
