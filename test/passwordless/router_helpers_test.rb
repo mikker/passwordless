12b30a57a78f22e57e57fc58d17ebcd822eb7b51
Henrik Bjørnskov <19725+henrikbjorn@users.noreply.github.com>
2023-09-13 19:33:51 +0200

    i think the controller test is there

--- test/passwordless/router_helpers_test.rb
+++ test/passwordless/router_helpers_test.rb
@@ -61,95 +61,46 @@ module Passwordless
 
       # GET /:passwordless_for/sign_in
       assert_recognizes(
-        {controller: "passwordless/sessions", action: "new"}.merge(defaults),
+        {controller: "admin/sessions", action: "new"}.merge(defaults),
         {method: :get, path: "/admins/sign_in"},
         defaults
       )
 
       # POST /:passwordless_for/sign_in
       assert_recognizes(
-        {controller: "passwordless/sessions", action: "create"}.merge(defaults),
+        {controller: "admin/sessions", action: "create"}.merge(defaults),
         {method: :post, path: "/admins/sign_in", params: {passwordless: {email: "a@a"}}},
         defaults
       )
 
       # GET /:passwordless_for/sign_in/:id/:token
       assert_recognizes(
-        {controller: "passwordless/sessions", action: "confirm", id: "123", token: "abc"}.merge(defaults),
+        {controller: "admin/sessions", action: "confirm", id: "123", token: "abc"}.merge(defaults),
         {method: :get, path: "/admins/sign_in/123/abc"},
         defaults
       )
 
       # PATCH /:passwordless_for/sign_in/:id
       assert_recognizes(
-        {controller: "passwordless/sessions", action: "update", id: "123"}.merge(defaults),
+        {controller: "admin/sessions", action: "update", id: "123"}.merge(defaults),
         {method: :patch, path: "/admins/sign_in/123"},
         defaults
       )
 
       # DELETE /:passwordless_for/sign_out
       assert_recognizes(
-        {controller: "passwordless/sessions", action: "destroy"}.merge(defaults),
+        {controller: "admin/sessions", action: "destroy"}.merge(defaults),
         {method: :delete, path: "/admins/sign_out"},
         defaults
       )
 
       # GET /:passwordless_for/sign_out
       assert_recognizes(
-        {controller: "passwordless/sessions", action: "destroy"}.merge(defaults),
+        {controller: "admin/sessions", action: "destroy"}.merge(defaults),
         {method: :get, path: "/admins/sign_out"},
         defaults
       )
     end
 
-    test("map sign in with custom controller") do
-      defaults = {
-        authenticatable: "admin",
-        resource: "admins",
-        controller: "custom_sessions",
-      }
-
-      # GET /:passwordless_for/sign_in
-      assert_recognizes(
-        {controller: "custom_sessions", action: "new"}.merge(defaults),
-        {method: :get, path: "/admins/sign_in"},
-        defaults
-      )
-
-      # POST /:passwordless_for/sign_in
-      assert_recognizes(
-        {controller: "custom_sessions", action: "create"}.merge(defaults),
-        {method: :post, path: "/admins/sign_in", params: {passwordless: {email: "a@a"}}},
-        defaults
-      )
-
-      # GET /:passwordless_for/sign_in/:id/:token
-      assert_recognizes(
-        {controller: "custom_sessions", action: "confirm", id: "123", token: "abc"}.merge(defaults),
-        {method: :get, path: "/admins/sign_in/123/abc"},
-        defaults
-      )
-
-      # PATCH /:passwordless_for/sign_in/:id
-      assert_recognizes(
-        {controller: "custom_sessions", action: "update", id: "123"}.merge(defaults),
-        {method: :patch, path: "/admins/sign_in/123"},
-        defaults
-      )
-
-      # DELETE /:passwordless_for/sign_out
-      assert_recognizes(
-        {controller: "custom_sessions", action: "destroy"}.merge(defaults),
-        {method: :delete, path: "/admins/sign_out"},
-        defaults
-      )
-
-      # GET /:passwordless_for/sign_out
-      assert_recognizes(
-        {controller: "custom_sessions", action: "destroy"}.merge(defaults),
-        {method: :get, path: "/admins/sign_out"},
-        defaults
-      )
-    end
   end
 end
