bce0d42477fab6cbb9102a47ac6fe1b6f88399e8
Mikkel Malmberg <mikkel@brnbw.com>
2023-10-12 10:07:11 +0200

    Fix fixture_path deprecation warning (#165)

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_085550) do
+ActiveRecord::Schema[7.1].define(version: 2023_06_21_085550) do
   create_table "passwordless_sessions", force: :cascade do |t|
     t.string "authenticatable_type"
     t.integer "authenticatable_id"
