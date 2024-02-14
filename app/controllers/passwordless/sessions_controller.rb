d4ae47b819634cbc36df150e7df5a3902cb19c85
andreas kundig <andreas.kuendig@gmx.ch>
2024-02-14 19:12:54 +0100

    revert changes involving params[:locale]

--- app/controllers/passwordless/sessions_controller.rb
+++ app/controllers/passwordless/sessions_controller.rb
@@ -30,8 +30,7 @@ module Passwordless
           Passwordless.context.path_for(
             @session,
             id: @session.to_param,
-            action: "show",
-            **locale_options
+            action: "show"
           ),
           flash: {notice: I18n.t("passwordless.sessions.create.email_sent")}
         )
@@ -250,11 +249,6 @@ module Passwordless
 
     def passwordless_session_params
       params.require(:passwordless).permit(:token, authenticatable_class.passwordless_email_field)
-    end
-
-    def locale_options
-      locale = params[:locale]
-      locale ? { locale: locale } : {}
     end
   end
 end
