b5e76183e13c7dc57e3819c0380d9cc9e56ed13f
Mikkel Malmberg <mikkel@brnbw.com>
2024-04-25 10:25:03 +0200

    🔨

--- db/migrate/20171104221735_create_passwordless_sessions.rb
+++ db/migrate/20171104221735_create_passwordless_sessions.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreatePasswordlessSessions < ActiveRecord::Migration
+class CreatePasswordlessSessions < ActiveRecord::Migration[6.0]
   def change
     create_table(:passwordless_sessions) do |t|
       t.belongs_to(
