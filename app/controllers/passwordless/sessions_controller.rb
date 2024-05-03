58268055549372b18a7df49975206ad9c46306ff
Dakota Schramm <william.schramm11@gmail.com>
2024-05-03 16:32:34 -0500

    Remove whitespace. Revert schema update

--- app/controllers/passwordless/sessions_controller.rb
+++ app/controllers/passwordless/sessions_controller.rb
@@ -203,7 +203,7 @@ module Passwordless
     def handle_resource_not_found
       if Passwordless.config.paranoid
         @resource = authenticatable_class.new(email: normalized_email_param)
-        @skip_after_session_save_callback = true 
+        @skip_after_session_save_callback = true
       else
         raise(
           ActiveRecord::RecordNotFound,
@@ -215,7 +215,7 @@ module Passwordless
     def call_after_session_save
       if @skip_after_session_save_callback
         if Passwordless.config.send_paranoid_email
-          Mailer.unknown_address(@session).deliver_now 
+          Mailer.unknown_address(@session).deliver_now
         end
       elsif Passwordless.config.after_session_save.arity == 2
         Passwordless.config.after_session_save.call(@session, request)
