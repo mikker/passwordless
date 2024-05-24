9752f810651f4f3a7265878205cf0032ded33ff3
Tom de Grunt <tom@degrunt.nl>
2024-05-24 15:44:35 +0200

    Add authenticated, make proc optional

--- lib/passwordless/constraint.rb
+++ lib/passwordless/constraint.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
-require "passwordless/controller_helpers"
+require 'passwordless/controller_helpers'
 
 module Passwordless
   class Constraint
     include ControllerHelpers
 
-    attr_reader :authenticatable_type, :session, :lambda
+    attr_reader :authenticatable_type, :authenticated, :lambda, :session
 
-    def initialize(authenticatable_type, lambda)
+    def initialize(authenticatable_type, lambda = -> { true }, authenticated: true)
       @authenticatable_type = authenticatable_type
+      @authenticated = authenticated
       @lambda = lambda
     end
 
     def matches?(request)
       @session = request.session
       authenticatable = authenticate_by_session(authenticatable_type)
-      authenticatable.present? && lambda.call(authenticatable)
+      authenticatable.present? == authenticated && lambda.call(authenticatable)
     end
   end
 end
